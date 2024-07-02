514597d7805a0bc03393b9654fbda8808cc4df15
Hoangnhat2711 <tranhoangnhatkp@gmail.com>
2024-07-02 19:20:20 +0700

    baitap7/2

---
+++ CRUDJAVA/src/com/company/File.java
@@ -0,0 +1,83 @@
+//package com.company;
+//
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//
+//public class File {
+//    public static void main(String[] args) {
+//        try {
+//            FileReader reader = new FileReader("src/com/company/Hi.txt");
+//            FileWriter writer = new FileWriter("Output_Training_01.txt");
+//
+//            int line;
+//            String str = "";
+//            while ((line = reader.read()) != -1) {
+//                System.out.print((char) line);
+//                str += (char) line;
+//            }
+//            writer.write(str);
+//            reader.close();
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File {
+
+    public static void main(String[] args) {
+
+        addStudent("Nhật, 123456");
+
+        try {
+            FileReader reader = new FileReader("src/com/company/Hi.txt");
+            int line;
+            StringBuilder str = new StringBuilder();
+
+
+            while ((line = reader.read()) != -1) {
+                str.append((char) line);
+            }
+            reader.close();
+            System.out.println(str.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public static void addStudent(String student) {
+        try {
+            FileReader reader = new FileReader("src/com/company/Hi.txt");
+            StringBuilder str = new StringBuilder();
+            int line;
+
+
+            while ((line = reader.read()) != -1) {
+                str.append((char) line);
+            }
+            reader.close();
+
+
+            str.append(student).append("\n");
+
+            FileWriter writer = new FileWriter("src/com/company/Hi.txt");
+            writer.write(str.toString());
+            writer.close();
+
+            System.out.println("Sinh viên đã thêm: " + student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }}
+
+
--- CRUDJAVA/src/com/company/StudentManagement.java
+++ CRUDJAVA/src/com/company/StudentManagement.java
@@ -1,95 +1,95 @@
-package com.company;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class StudentManagement {
-    private List<Student> students;
-
-    public StudentManagement() {
-        this.students = new ArrayList<>();
-    }
-
-    public void addStudent(Student student) {
-        students.add(student);
-    }
-
-    public void deleteStudent(String id) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getId() == id) {
-                students.remove(i);
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy sinh viên có id: " + id);
-    }
-
-    public void updateStudent(String id, String newName, String newClassName, int newAge) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                student.setName(newName);
-                student.setClassName(newClassName);
-                student.setAge(newAge);
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy sinh viên có id: " + id);
-    }
-
-    public void displayListStudents() {
-        for (Student student : students) {
-            System.out.println(student);
-        }
-    }
-
-    public void filterListStudentsByName(String substring) {
-        for (Student student : students) {
-            if (student.getName().contains(substring)) {
-                System.out.println(student);
-            }
-        }
-    }
-
-    public void filterListStudentsByAge(int age) {
-        for (Student student : students) {
-            if (student.getAge() == age) {
-                System.out.println(student);
-            }
-        }
-    }
-
-
-    public void displayStudentByName(String name) {
-        for (Student student : students) {
-            if (student.getName()==name) {
-                System.out.println(student);
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy sinh viên có tên: " + name);
-    }
-
-    public void displayStudentByID(String id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                System.out.println(student);
-                return;
-            }
-        }
-        System.out.println("Không tìm thấy sinh viên có id: " + id);
-    }
-//    public void displayMaxId(){
-//        int Max = 0;
-//        for (Student student : students){
-//            if (student.getId()>Max){
-//                Max = student.getId();
+//package com.company;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class StudentManagement {
+//    private List<Student> students;
+//
+//    public StudentManagement() {
+//        this.students = new ArrayList<>();
+//    }
+//
+//    public void addStudent(Student student) {
+//        students.add(student);
+//    }
+//
+//    public void deleteStudent(String id) {
+//        for (int i = 0; i < students.size(); i++) {
+//            if (students.get(i).getId() == id) {
+//                students.remove(i);
+//                return;
 //            }
 //        }
-//        for (Student student : students){
-//            if (student.getId()==Max){
+//        System.out.println("Không tìm thấy sinh viên có id: " + id);
+//    }
+//
+//    public void updateStudent(String id, String newName, String newClassName, int newAge) {
+//        for (Student student : students) {
+//            if (student.getId() == id) {
+//                student.setName(newName);
+//                student.setClassName(newClassName);
+//                student.setAge(newAge);
+//                return;
+//            }
+//        }
+//        System.out.println("Không tìm thấy sinh viên có id: " + id);
+//    }
+//
+//    public void displayListStudents() {
+//        for (Student student : students) {
+//            System.out.println(student);
+//        }
+//    }
+//
+//    public void filterListStudentsByName(String substring) {
+//        for (Student student : students) {
+//            if (student.getName().contains(substring)) {
 //                System.out.println(student);
 //            }
 //        }
+//    }
+//
+//    public void filterListStudentsByAge(int age) {
+//        for (Student student : students) {
+//            if (student.getAge() == age) {
+//                System.out.println(student);
+//            }
 //        }
-//}
-
+//    }
+//
+//
+//    public void displayStudentByName(String name) {
+//        for (Student student : students) {
+//            if (student.getName()==name) {
+//                System.out.println(student);
+//                return;
+//            }
+//        }
+//        System.out.println("Không tìm thấy sinh viên có tên: " + name);
+//    }
+//
+//    public void displayStudentByID(String id) {
+//        for (Student student : students) {
+//            if (student.getId() == id) {
+//                System.out.println(student);
+//                return;
+//            }
+//        }
+//        System.out.println("Không tìm thấy sinh viên có id: " + id);
+//    }
+////    public void displayMaxId(){
+////        int Max = 0;
+////        for (Student student : students){
+////            if (student.getId()>Max){
+////                Max = student.getId();
+////            }
+////        }
+////        for (Student student : students){
+////            if (student.getId()==Max){
+////                System.out.println(student);
+////            }
+////        }
+////        }
+////}
+//
